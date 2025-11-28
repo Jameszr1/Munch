dec53776902580b7037dc259288d68d3a4f7744a
midhun kumar <midh0001@student.monash.edu>
2025-11-28 13:27:41 +1100

    feat: add JWT token endpoints for user authentication

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
-
-url_patterns = [
+urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path('users/', views.UserView.as_view(), name='user-list-create'),
 ]
